4988082ff2516359610e3b9c2d35c384aa57d296
kanik155 <taketani@lastmile-works.com>
2021-06-25 22:17:42 +0900

    update engine

---
+++ engine/__settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1182829.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': true,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 50359048, 50359051, 50359158, 50360152, 50423744, 50423871 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/50378239/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/50378240/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/50378238/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/50378308/1/basis.wasm.js', 'wasmUrl' : 'files/assets/50378309/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/50378307/1/basis.js', 'preload' : false},
+];
